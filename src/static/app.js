9d6ea5766c9a34dd79b236a516013a97544a95e6
skneyte <72237082+skneyte@users.noreply.github.com>
2026-01-14 04:55:44 +0000

    se agrego el test

--- src/static/app.js
+++ src/static/app.js
@@ -95,6 +95,7 @@ document.addEventListener("DOMContentLoaded", () => {
         messageDiv.textContent = result.message;
         messageDiv.className = "success";
         signupForm.reset();
+        fetchActivities(); // Actualiza la lista de actividades y participantes
       } else {
         messageDiv.textContent = result.detail || "An error occurred";
         messageDiv.className = "error";
